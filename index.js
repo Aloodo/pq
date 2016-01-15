5aa74b7a45b0452d5b06b973b60eb1bd4190d96d
Don Marti <dmarti@zgp.org>
2016-01-15 09:18:09 -0800

    Remove unsafe use of non-SDK interface
    No more dialog.

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-var { Cc, Ci } = require('chrome');
 var simpleStorage = require('sdk/simple-storage');
-var utils = require('sdk/window/utils');
-var tabs = require("sdk/tabs");
 var prefSvc = require("sdk/preferences/service");
 var { PrefsTarget } = require("sdk/preferences/event-target");
-var window = utils.getMostRecentBrowserWindow();
 
 var tpPref = 'privacy.trackingprotection.enabled';
 var sessionPref = 'network.cookie.thirdparty.sessionOnly';
@@ -17,10 +13,6 @@ var sessionPref = 'network.cookie.thirdparty.sessionOnly';
 var cookiePref = 'network.cookie.cookieBehavior';
 
 var pt = PrefsTarget();
-
-var prompts =
-  Cc["@mozilla.org/embedcomp/prompt-service;1"].
-      getService(Ci.nsIPromptService);
 
 var runTPtest = {value: false};
 var stg = simpleStorage.storage;
@@ -68,16 +60,6 @@ function doConfig() {
 	/* only change cookie behavior if wide-open */
 	if (prefSvc.get(cookiePref, 0) == 0) {
 		prefSvc.set(cookiePref, 3);
-	}
-
-	prompts.alertCheck(window, "pq",
-	"Firefox Tracking Protection and third-party cookie " +
-	"protection are now enabled. " +
-	"Thank you for helping to protect the web sites " +
-	"you use from data leakage and fraud.",
-	"Run tracking test?", runTPtest);
-	if (runTPtest.value) {
-		tabs.open('http://www.aloodo.org/test/start/');
 	}
 }
 
